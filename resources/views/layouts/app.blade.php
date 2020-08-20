626302977ea960167f533877338561305af1074d
GuiPeras <57112157+GuiPeras@users.noreply.github.com>
2020-08-20 16:13:32 +0200

    adding view footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,8 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+            <div class="container ">
+                <a class="navbar-brand " href="{{ url('/') }}">
                     {{ config('app.name', 'FBN') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
@@ -36,7 +36,7 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="nav justify-content-center ">
 
                         <li class="nav-item">
                             <a class="nav-link" href="/about">About</a>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<footer class="page-footer font-small blue">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
