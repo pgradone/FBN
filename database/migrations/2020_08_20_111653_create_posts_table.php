f108fde60c3fcc1aa5c0945fb629c440106c238f
Potito Gradone <pgradone@gmail.com>
2020-08-20 13:28:21 +0200

    created post table for migration

--- database/migrations/2020_08_20_111653_create_posts_table.php
+++ database/migrations/2020_08_20_111653_create_posts_table.php
@@ -13,8 +13,17 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('posts_', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('parent_post_id');
+            $table->foreignId('author_id');
+            $table->string('title',2);
+            $table->string('content',2);
+            $table->string('summary',2);
+            $table->string('reference',2);
+            $table->unsignedSmallInteger('post_type_id');
+            $table->unsignedSmallInteger('language_id');
+
             $table->timestamps();
         });
     }
