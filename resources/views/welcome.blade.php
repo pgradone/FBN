c6d607edbf8deb2d3c88e3290ee38947ceb256da
GuiPeras <57112157+GuiPeras@users.noreply.github.com>
2020-08-21 15:49:23 +0200

    header and also recipe containers added to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,59 @@
 @section('content')
 
 <body>
+
+    <div class="container d-flex justify-content-around my-5">
+        <div class="row">
+            <div class="col-md d-flex align-items-center">
+                <div>
+                    <h1><strong>Facts-Based Nutrition</strong></h1>
+                    <h5>A healthy Way </h5>
+                    <button type="button" class="btn btn-success">Join Now</>
+                </div>
+            </div>
+            <div class="col-md ">
+                <img class="img-thumbnail"
+                    src="https://images.unsplash.com/photo-1503146234394-631200675614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                    alt="picture" srcset="" style="max-width: 18rem;">
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
     <div class="d-flex justify-content-center">
+        <h1><strong>Recipes</strong></h1>
+    </div>
+    <hr class="my-4 " style="width: 18rem; border: dashed ; ">
+    <div class="d-flex justify-content-center">
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.unsplash.com/photo-1524859330668-c357331384f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                class="card-img-top" alt="...">
+            <div class="card-body">
 
-        <div class="d-flex justify-content-between ">
-
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                <a href="#" class="btn btn-primary">name of recipe</a>
             </div>
         </div>
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                class="card-img-top" alt="...">
+            <div class="card-body">
+
+                <a href="#" class="btn btn-primary">name of recipe</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                class="card-img-top" alt="...">
+            <div class="card-body">
+
+                <a href="#" class="btn btn-primary">name of recipe</a>
+            </div>
+        </div>
+
     </div>
 </body>
 @endsection
