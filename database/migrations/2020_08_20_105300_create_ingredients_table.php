ac8c372f78ca1ccf8b19845693b25e6cb2a37b12
pgradone <pgradone@gmail.com>
2020-08-22 11:49:35 +0200

    abandon food categories, add Plant/Animal based origin

--- database/migrations/2020_08_20_105300_create_ingredients_table.php
+++ database/migrations/2020_08_20_105300_create_ingredients_table.php
@@ -16,9 +16,10 @@ class CreateIngredientsTable extends Migration
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
             $table->string('nutriscore',2);
-            $table->foreignId('food_category_id')->references('id')->on('food_categories');
-            $table->foreignId('foodgroup_id')->references('id')->on('foodgroups');
+            $table->string('origin',2)->comment('Plant or Animal based');
+            // $table->foreignId('food_category_id')->references('id')->on('food_categories');
             $table->string('picture',255);
+            $table->foreignId('foodgroup_id')->references('id')->on('foodgroups');
             $table->timestamps();
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
