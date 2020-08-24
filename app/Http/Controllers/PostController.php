6d63b85697dafe2c5279bcef8e51920f80f275f3
faballaC <faballacisse@outlook.fr>
2020-08-24 14:39:08 +0200

    show all posts => PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+  public function index()
+  {
+    //fetch 5 posts from database which are active and latest
+    $posts = Posts::where('active', 1)->orderBy('created_at', 'desc')->paginate(5);
+    //page heading
+    $title = 'Latest Posts';
+    //return home.blade.php template from resources/views folder
+    return view('home')->withPosts($posts)->withTitle($title);
+  }
 }
