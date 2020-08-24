00bce6128ac467b262bcbaaf4eb11000e7b93dee
Potito Gradone <pgradone@gmail.com>
2020-08-24 15:27:03 +0200

    DB structure final and food origin

--- database/seeds/foods_denormalize.sql
+++ database/seeds/foods_denormalize.sql
@@ -3,18 +3,10 @@ insert into foodgroups (id) select distinct group_id from foods order by group_i
 /* subgroup IDs and their parent group_ids */
 insert into foodgroups (id, parent_group_id) select distinct sub_group_id, group_id from foods where sub_group_id > 27 order by sub_group_id;
 
-/* ingredient names in 8 languages */
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 2, food_name_EN FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 3, food_name_FR FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 4, food_name_DE FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 5, food_name_PT FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 6, food_name_IT FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 7, food_name_ES FROM foods;
-insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 8, food_name_NL FROM foods;
-
 /* main group names in 8 languages */
 insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct group_id, 2, `group_EN` FROM `foods` order by group_id;
 insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct group_id, 3, `group_FR` FROM `foods` order by group_id;
+
 insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct group_id, 4, `group_DE` FROM `foods` order by group_id;
 insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct group_id, 5, `group_PT` FROM `foods` order by group_id;
 insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct group_id, 6, `group_IT` FROM `foods` order by group_id;
@@ -33,7 +25,29 @@ insert into foodgroup_names (foodgroup_id, language_id, name) SELECT distinct su
 /* Insert Ingredients with their belonging foodgroups_id */
 INSERT INTO ingredients (id, foodgroup_id) SELECT ID, sub_group_id FROM foods order by ID;
 
+/* ingredient names in 8 languages */
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 2, food_name_EN FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 3, food_name_FR FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 4, food_name_DE FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 5, food_name_PT FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 6, food_name_IT FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 7, food_name_ES FROM foods;
+insert into ingredients_names (ingredient_id, language_id, name) SELECT id, 8, food_name_NL FROM foods;
+
 /* View original foods table with a query */
+SELECT i.id, i.foodgroup_id, in_en.name food_name_EN, in_fr.name food_name_FR,
+	fgn_en.name sub_group_EN, fgn_fr.name sub_group_FR
+	from ingredients i 
+left join ingredients_names in_en on i.id = in_en.ingredient_id and in_en.language_id = 2
+left join ingredients_names in_fr on i.id = in_fr.ingredient_id and in_fr.language_id = 3
+
+left join foodgroups fg_sub on i.foodgroup_id = fg_sub.id
+left join foodgroup_names fgn_en on fg_sub.id = fgn_en.foodgroup_id and fgn_en.language_id = 2
+left join foodgroup_names fgn_fr on fg_sub.id = fgn_fr.foodgroup_id and fgn_fr.language_id = 3
+
+
+/* -----------------------*/
+
 SELECT i.id, i.foodgroup_id, in_en.name from ingredients i 
 left join ingredients_names in_en on i.id = in_en.ingredient_id
 
