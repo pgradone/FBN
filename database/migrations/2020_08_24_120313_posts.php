b58ab65ee0f42c845202a00e67d119a3eec64150
faballaC <faballacisse@outlook.fr>
2020-08-24 14:13:18 +0200

    created table for posts

--- database/migrations/2020_08_24_120313_posts.php
+++ database/migrations/2020_08_24_120313_posts.php
@@ -6,23 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 class Posts extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    //blog table
+    Schema::create('posts', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('author_id');
+      $table->foreign('author_id')
+        ->references('id')->on('users')
+        ->onDelete('cascade');
+      $table->string('title')->unique();
+      $table->text('body');
+      $table->string('slug')->unique();
+      $table->boolean('active');
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    // drop blog table
+    Schema::drop('posts');
+  }
 }
