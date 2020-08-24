4a5b3444b4513e9e2b04c88f22d9b871a5097fd5
faballaC <faballacisse@outlook.fr>
2020-08-24 14:56:18 +0200

    create comment => CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+  public function store(Request $request)
+  {
+    //on_post, from_user, body
+    $input['from_user'] = $request->user()->id;
+    $input['on_post'] = $request->input('on_post');
+    $input['body'] = $request->input('body');
+    $slug = $request->input('slug');
+    Comments::create($input);
+    return redirect($slug)->with('message', 'Comment published');
+  }
 }
