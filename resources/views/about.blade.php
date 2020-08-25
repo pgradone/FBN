b770caede4ef427628a21b2a5b151d215201aefa
Alex the Emperor Penguin <JeffCigrand@me.com>
2020-08-25 16:21:13 +0200

    Jef (#26)
    * It fucking wörks nau
    
    * Dev (#24)
    
    * add seeders to DatabaseSeeder class
    
    * seeders not working
    
    * get seeds from json files
    
    * Seeding works
    
    * include ingredient origin and nutriscore
    
    * add picture from spoonacular into ingredients
    
    * creation of custom css file and also some little changes to the home page.
    
    * It fucking wörks nau (#23)
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    
    * Dev (#25)
    
    * add seeders to DatabaseSeeder class
    
    * seeders not working
    
    * get seeds from json files
    
    * Seeding works
    
    * include ingredient origin and nutriscore
    
    * add picture from spoonacular into ingredients
    
    * creation of custom css file and also some little changes to the home page.
    
    * It fucking wörks nau (#23)
    
    * food denormalizing
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    
    * centered tiles
    
    * Pages prep
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,10 @@
 </head>
 
 <body>
-    <h1>this is the about us page</h1>
+    <h1 class="text-center">this is the about us page</h1>
+    <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ea soluta doloremque officiis omnis culpa autem magni iure quasi in! Accusantium tenetur, eius temporibus adipisci facilis aspernatur veniam? Quisquam, quas.
+    </p>
 </body>
 
 </html>
