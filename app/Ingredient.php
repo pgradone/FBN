5d1a01b655e1a2b4532a5cdc23c1d7649584f430
Alex the Emperor Penguin <JeffCigrand@me.com>
2020-08-26 11:30:18 +0200

    Dev (#37)
    * add seeders to DatabaseSeeder class
    
    * seeders not working
    
    * get seeds from json files
    
    * Seeding works
    
    * include ingredient origin and nutriscore
    
    * add picture from spoonacular into ingredients
    
    * creation of custom css file and also some little changes to the home page.
    
    * It fucking wörks nau (#23)
    
    * food denormalizing
    
    * new recipe section and also background image on the header
    
    * Jef (#26)
    
    * It fucking wörks nau
    
    * Dev (#24)
    
    * add seeders to DatabaseSeeder class
    
    * seeders not working
    
    * get seeds from json files
    
    * Seeding works
    
    * include ingredient origin and nutriscore
    
    * add picture from spoonacular into ingredients
    
    * creation of custom css file and also some little changes to the home page.
    
    * It fucking wörks nau (#23)
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    
    * Dev (#25)
    
    * add seeders to DatabaseSeeder class
    
    * seeders not working
    
    * get seeds from json files
    
    * Seeding works
    
    * include ingredient origin and nutriscore
    
    * add picture from spoonacular into ingredients
    
    * creation of custom css file and also some little changes to the home page.
    
    * It fucking wörks nau (#23)
    
    * food denormalizing
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    
    * centered tiles
    
    * Pages prep
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    
    * add Ingredient with eloquent
    
    * added some good pictures for the background of the header and also some tweaks on the header with bootstrap
    
    * add Ingredients Controller, view and routes
    
    * add Post and PostController main objects
    
    * check meaning of Auth::routes()
    
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>

---
+++ app/Ingredient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    //
+    protected $attributes = [
+        'language_id' => 2,
+    ];
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/faq">FAQ</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/ingredients">FAQ</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
