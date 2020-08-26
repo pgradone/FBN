af08240339688f7903c38999b342d8abcfe6ed0f
GuiPeras <57112157+GuiPeras@users.noreply.github.com>
2020-08-26 11:08:08 +0200

    adding  comments to section recipes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
 
 <!-- Recipes title and HR  -->
 <div class=" d-flex justify-content-center" style="margin-top:3rem;">
-    <h1><strong>Recipes</strong></h1>
+    <h1><strong>Recipes :</strong></h1>
 </div>
 <hr class="my-4 " style="width: 15rem;border-top: 5px dotted green;">
 
@@ -29,57 +29,63 @@
     <div class="recipesContainer">
         <div class="row">
             <div class="col-md-4 col-sm-12">
+                <!-- Recipe Card 1 -->
                 <div class="card mb-4 box-shadow">
                     <img class="card-img-top"
                         src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                         alt="Card image cap">
                     <div class="card-body">
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is the description of the recipe</p>
                         <div class="d-flex justify-content-between align-items-center">
+                            <!-- buttons on the bottom on the right  -->
                             <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <!-- View only for admin itself -->
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
+                            <!-- time to make the recipe -->
                             <small class="text-muted">9 mins</small>
                         </div>
                     </div>
                 </div>
             </div>
             <div class=" col-md-4 col-sm-12">
+                <!-- Recipe Card 2 -->
                 <div class="card mb-4 box-shadow">
                     <img class="card-img-top"
-                        src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                        src="https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                         alt="Card image cap">
                     <div class="card-body">
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is the description of the recipe</p>
                         <div class="d-flex justify-content-between align-items-center">
+                            <!-- buttons on the bottom on the right  -->
                             <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <!-- View only for admin itself -->
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
+                            <!-- time to make the recipe -->
                             <small class="text-muted">9 mins</small>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12">
+                <!-- Recipe Card 3 -->
                 <div class="card mb-4 box-shadow">
                     <img class="card-img-top"
-                        src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                        src="https://images.unsplash.com/photo-1509249270761-2f410c3c47dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
                         alt="Card image cap">
                     <div class="card-body">
-                        <p class="card-text">This is a wider card with supporting text below as a natural
-                            lead-in to
-                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text">This is the description of the recipe</p>
                         <div class="d-flex justify-content-between align-items-center">
+                            <!-- buttons on the bottom on the right  -->
                             <div class="btn-group">
                                 <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                <!-- View only for admin itself -->
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                             </div>
+                            <!-- time to make the recipe -->
                             <small class="text-muted">9 mins</small>
                         </div>
                     </div>
