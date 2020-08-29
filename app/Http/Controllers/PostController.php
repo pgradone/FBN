f70c63229a865d3c00dfe9674168bede60f36835
Potito Gradone <pgradone@gmail.com>
2020-08-29 22:17:51 +0200

    in PostController, use Post instead of Posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
-use App\Posts;
+use App\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostFormRequest;
 
@@ -14,7 +14,7 @@ class PostController extends Controller
   public function index()
   {
     //fetch 5 posts from database which are active and latest
-    $posts = Posts::where('active', 1)->orderBy('created_at', 'desc')->paginate(5);
+    $posts = Post::where('active', 1)->orderBy('created_at', 'desc')->paginate(5);
     //page heading
     $title = 'Latest Posts';
     //return home.blade.php template from resources/views folder
@@ -33,12 +33,12 @@ class PostController extends Controller
 
   public function store(PostFormRequest $request)
   {
-    $post = new Posts();
+    $post = new Post();
     $post->title = $request->get('title');
     $post->body = $request->get('body');
     $post->slug = Str::slug($post->title);
 
-    $duplicate = Posts::where('slug', $post->slug)->first();
+    $duplicate = Post::where('slug', $post->slug)->first();
     if ($duplicate) {
       return redirect('new-post')->withErrors('Title already exists.')->withInput();
     }
@@ -57,7 +57,7 @@ class PostController extends Controller
 
   public function show($slug)
   {
-    $post = Posts::where('slug', $slug)->first();
+    $post = Post::where('slug', $slug)->first();
     if (!$post) {
       return redirect('/')->withErrors('requested page not found');
     }
@@ -67,7 +67,7 @@ class PostController extends Controller
 
   public function edit(Request $request, $slug)
   {
-    $post = Posts::where('slug', $slug)->first();
+    $post = Post::where('slug', $slug)->first();
     if ($post && ($request->user()->id == $post->author_id || $request->user()->is_admin()))
       return view('posts.edit')->with('post', $post);
     return redirect('/')->withErrors('you have not sufficient permissions');
@@ -77,11 +77,11 @@ class PostController extends Controller
   {
     //
     $post_id = $request->input('post_id');
-    $post = Posts::find($post_id);
+    $post = Post::find($post_id);
     if ($post && ($post->author_id == $request->user()->id || $request->user()->is_admin())) {
       $title = $request->input('title');
       $slug = Str::slug($title);
-      $duplicate = Posts::where('slug', $slug)->first();
+      $duplicate = Post::where('slug', $slug)->first();
       if ($duplicate) {
         if ($duplicate->id != $post_id) {
           return redirect('edit/' . $post->slug)->withErrors('Title already exists.')->withInput();
@@ -113,7 +113,7 @@ class PostController extends Controller
   public function destroy(Request $request, $id)
   {
     //
-    $post = Posts::find($id);
+    $post = Post::find($id);
     if ($post && ($post->author_id == $request->user()->id || $request->user()->is_admin())) {
       $post->delete();
       $data['message'] = 'Post deleted Successfully';
