aa2ad11892cb8cd28d63246b29f5d9ff48115a18
Guilherme <gui_udemy1@protonmail.com>
2020-08-30 15:37:22 +0200

    navbar redesign gui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 
     <!-- Font-Awsome -->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 
     <!-- Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -22,7 +21,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
     <!-- Styles from APP.CSS -->
-    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
@@ -52,73 +51,73 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="">FBN</a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="{{ url('/') }}">Home</a>
+    <nav class="navbar custom-navbar navbar-expand-md shadow-sm">
+        <div class="container-fluid ">
+            <a class="navbar-brand custom-navbar-brand " href="{{ url('/') }}">
+                {{ config('app.name', 'FBN') }}
+            </a>
+            <button class="navbar-toggler ml-auto custom-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav justify-content-center ">
+                    <li class="nav-item ">
+                        <a class="nav-link custom-nav-item" href="/about">About</a>
                     </li>
                     <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                        <a href="/recipes">Recipes</a>
+                        <a class="nav-link custom-nav-item" href="/recipes">Recipes</a>
                     </li>
                     <li class="nav-item ">
-                        <a href="/blog">Blog</a>
+                        <a class="nav-link custom-nav-item " href="/blog">Blog</a>
                     </li>
                     <li class="nav-item ">
-                        <a href="/faq">FAQ</a>
+                        <a class="nav-link custom-nav-item " href="/faq">FAQ</a>
                     </li>
                     <li class="nav-item">
-                        <a href="/ingredients">Ingredients</a>
+                        <a class="nav-link custom-nav-item" href="/ingredients">Ingredients</a>
                     </li>
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                    <li>
-                        <a href="{{ url('/auth/login') }}">Login</a>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link custom-nav-item" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
-                    <li>
-                        <a href="{{ url('/auth/register') }}">Register</a>
-                    </li>
-                    @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                            aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            @if (Auth::user()->can_post())
-                            <li>
-                                <a href="{{ url('/new-post') }}">Add a new post</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/user/'.Auth::id().'/posts') }}">My Posts</a>
-                            </li>
-                            @endif
-                            <li>
-                                <a href="{{ url('/user/'.Auth::id()) }}">My Profile</a>
-                            </li>
-                            <li>
-                                <a href="{{ url('/auth/logout') }}">Logout</a>
-                            </li>
-                        </ul>
+                    @if (Route::has('register'))
+                    <li class="nav-item ">
+                        <a class="nav-link custom-nav-item" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
                     @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
                 </ul>
             </div>
         </div>
     </nav>
+
     <div class="container">
         @if (Session::has('message'))
         <div class="flash alert-info">
@@ -151,12 +150,15 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <p>Copyright © 2020 | <a href="">FBN</a></p>
-            </div>
-        </div>
     </div>
+    <footer id="sticky-footer" class="navbar  custom-navbar navbar-expand-md">
+        <div class="container text-center">
+            <h2 class="footer-copyright text-center ">
+                © 2020 Powered by:<a href="http://www.numericall.fr/">Numericall</a>
+            </h2>
+        </div>
+    </footer>
+
     <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
