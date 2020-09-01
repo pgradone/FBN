9091eb2213bfab80f8080d53f7241e32a554367c
GuiPeras <57112157+GuiPeras@users.noreply.github.com>
2020-09-01 10:30:27 +0200

    dropdown of user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
                         </li>
                         @endif
                         @else
-                        <li class=" nav-item dropdown d-flex  justify-content-center align-center">
+                        <!-- <li class=" nav-item dropdown d-flex  justify-content-center align-center">
                             <div class="custom-nav-item-profile text-center ">
                                 <a id="navbarDropdown" class="nav-link custom-navbar-profile-user" href="#"
                                     role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
@@ -100,6 +100,40 @@
 
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                              document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
+
+                            </div>
+
+                        </li> -->
+
+
+
+
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span> </span> <span class="caret"> </span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                <a class="dropdown-item" href="{{ url('/user/'.Auth::id()) }}"><span><i
+                                            class="fas fa-users-cog"> </i></span> My Profile </a>
+
+                                <a class="dropdown-item" href="{{ url('/user/'.Auth::id().'/posts') }}">My Posts</a>
+
+                                <a class="dropdown-item" href="{{ url('/new-post') }}">Add a new post <span
+                                        class="glyphicon glyphicon-cog pull-right"></span> </a>
+
+                                <a class="dropdown-item" class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                         document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
 
