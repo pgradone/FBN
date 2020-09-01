c59aac663e513c0d5c94027f96e3567a3623f1d4
Alex the Emperor Penguin <JeffCigrand@me.com>
2020-09-01 11:26:26 +0200

    Dev (#72)
    * adjusting the fonts and the style in the welcome page
    
    * fix bug on PostController
    
    * fix edit bug on blog editing
    
    * setup lang_ button for future ajax call
    
    * drop down and much more
    
    * faq page done with some questions
    
    * stable state reset
    
    * delete 6 problem files Post(s) and comments controllers , hone,balde and update-ingredient.blade
    
    * reset correct files to dev manually from pot2
    
    * reset the navbar manually - login options work
    
    * dropdown of user
    
    Co-authored-by: GuiPeras <57112157+GuiPeras@users.noreply.github.com>
    Co-authored-by: Potito Gradone <pgradone@gmail.com>
    Co-authored-by: Guilherme <gui_udemy1@protonmail.com>

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Comment;
 
 class CommentController extends Controller
@@ -16,76 +15,6 @@ class CommentController extends Controller
     $input['body'] = $request->input('body');
     $slug = $request->input('slug');
     Comment::create($input);
-    return redirect($slug)->with('message', 'Comment published');
+    return redirect('/blog/' . $input['on_post'] )->with('message', 'Comment published');
   }
-
-
-class CommentController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
 }
